2366553a797b85e08685dc124eae428f1fc87eb5
Ben Gottlieb <ben@standalone.com>
2021-06-27 21:22:24 -0500

    Initial Commit

---
+++ MazeGenerator/MazeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeGeneratorApp.swift
+//  MazeGenerator
+//
+//  Created by Ben Gottlieb on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct MazeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
